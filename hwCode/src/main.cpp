b2fd2dfd81f95f1a7f108afdd8d6ed9b902a790c
Zain <zain@accucalc.com>
2026-06-20 16:12:13 -0500

    code refactor

--- hwCode/src/main.cpp
+++ hwCode/src/main.cpp
@@ -6,18 +6,42 @@
  *
  * APS ReadySetCode GrowDuino
  * This code does the following
- *		- Measures Ambieint temperature and humidity using the DHT11/22
+ *		- Measures Ambient temperature and humidity using the DHT11/22
  *		- Measures soil moisture with a resistive soil moisture probe
  *		- uses the following information to water the plant
  *
- * This code is segmented to test/learn about things individually before connecting everything together
+ * TODO: Tadgh Exercise for the kids
+ *		explain that floats are decimal points written in binary
+ *		have kids encode 32 bit IEEE floats manually, to understand INF, NAN, and decimals
+ *		hopefully this will prep the kids to understand why the library uses NaN for errors
  *
- * I beg of thee, don't dump this into chatgpt i commented this out so a human can read it
+ * TODO: Tadgh explain RH scale and add to slides
+ *		for a specific temperature,
+ *		maximum amount of water air can hold is called the dew point (extra water in the air will condensate)
+ *		RH is the percentage of water in the air, more than 100% water begins to condensate
+ *		RH = mass water in air / mass water that can be in the air
  *
- * TODO: FIND BETTER NAME
- *		 FIND DISTRIBUTION LICENSE FOR THIS PROJECT, maybe apache?
- *		 UNIT TESTING
- *		 GARDUINO FORM FACTOR EXPANSION AND TURN INTO BULBASAUR
+ *	TODO: Tadgh explain Ohms Law
+ *		something to explain ohms law
+ *		electrons are in a super position of moving everywhere at once at 0V
+ *		applying a voltage shifts the electron waveform to move towards electron holes
+ *		when electrons encounter atoms and other electrons in their travels
+ *		they create electron interference waveforms (think the double slit experiment) and
+ *		the finite barrier problem (https://qm1.quantumtinkerer.tudelft.nl/8_finite-barrier/)
+ *		some electrons are reflected backwards, which creates resistance, V = IR
+ *
+ *
+ * Docs + links:
+ * parts documentation links
+ * https://www.mouser.com/datasheet/2/758/DHT11-Technical-Data-Sheet-Translated-Version-1143054.pdf
+ * https://www.onsemi.com/download/data-sheet/pdf/ss8050-d.pdf
+ *
+ * https://image-ppubs.uspto.gov/dirsearch-public/print/downloadPdf/5696671 << calculating water lost by TH sensors, pure IOT approach
+ * https://pmc.ncbi.nlm.nih.gov/articles/PMC8237332/ << ML approach to the project
+ * https://iwaponline.com/jh/article/26/12/3224/106261/Development-of-a-smart-irrigation-monitoring << edge compute approach
+ *
+ *
+ * TODO: GARDUINO FORM FACTOR EXPANSION AND TURN INTO BULBASAUR
  *		 PHOTODIODE AND NEOPIXEL EXPANSION
  *		 3DP CHECK VALVE
  *		 HAVE MORE VARIETY OF PLANTS TO TRY OUT
@@ -34,18 +58,12 @@
 #define RSTV_MSTR_PIN A0
 
 #define DHT_TYPE DHT11
+#define IS_VALID(x) (!isnan(x) && !isinf(x))
 
-
-/** MACROS for adjusting and tuning the watering algorithm
- * WATERING_TIME - defines how often to check dryness of soil and water plant (ms)
- * WATERING_PWR - Controls the pump power/flow rate, running full speed might create big splash (PWM 0-255)
- * WATERING_OTHER_TIME - Controls how long to water the plant for (ms)
- * WATERING_RESISTANE - arbitrary resistance threshold for deciding if WET or DRY, less resistance means dryer, (Ohms)
- */
-#define WATERING_TIME 3 * 3600 * 1000 // 3h * 3600 s/h * 1000 ms/s
-#define WATERING_PWR 50
-#define WATERING_OTHER_TIME 1000
-#define WATERING_RESISTANCE 85000
+// prgrm ctrl defines
+#define WATERING_PWR 100 // Controls the pump power/flow rate, running full speed might create big splash (PWM 0-255)
+// #define WATERING_OTHER_TIME 1000 // Controls how long to water the plant for (ms)
+// #define WATERING_RESISTANCE 85000 // arbitrary resistance threshold for deciding if WET or DRY, less resistance means dryer, (Ohms)
 
 DHT dht(DHT11_PIN, DHT_TYPE);
 
@@ -53,144 +71,149 @@ void setup() {
 	pinMode(PWM_PIN, OUTPUT);
 	dht.begin();
 
-	Serial.begin(9600);
-	Serial.println("Chicken");
+	Serial.begin(115200);
+	Serial.println("hello world");
+}
+
+/**
+ * Calculate the soil moisture percentage based on soil resistance.
+ * @param soilResistance resistance value of the soil in ohms
+ * @return soil moisture percentage constrained between 0 and 100
+ */
+unsigned long soilPercent(const float& soilResistance) {
+	// step 2: give values for DRY RESIST and WET RESIST in ohms, keep the UL at the end pls
+	const unsigned long DRYRESIST = 4000UL;
+	const unsigned long WETRESIST = 2700UL;
+	const unsigned long percentage = map(soilResistance, DRYRESIST, WETRESIST, 0UL, 40UL);
+	return max(0UL, min(percentage, 100UL));
+}
+
+/**
+ * take an average analog reading for a higher accuracy
+ * @param pinNum analog pin to read from
+ * @return analog value to return
+ */
+unsigned long avgReading(const int& pinNum) {
+
+	const unsigned long NUM_READINGS = 256UL;
+	// pick any power of 2 for fast division later
+
+	unsigned long sum = 0;
+	for (int i = 0; i < NUM_READINGS; i++) {
+		sum += analogRead(pinNum);
+	}
+
+	return sum / NUM_READINGS;
+}
+
+/**
+ * Calculates the resistance of the second resistor (R2) in a voltage divider circuit.
+ *
+ * @param reading The ADC reading, representing the voltage measured at the output of the voltage divider.
+ * @return The calculated resistance of the second resistor (R2) in ohms.
+ */
+float resistorDivCalc(const unsigned long& reading) {
+
+	const float R1 = 10000.f; // 10K ohm resistor that we are using
+	const float Vin = 5.f; // voltage level of the resistor divider
+	const float Vout = reading * (Vin / 1023.f); // 1023 is the max value that can come out of the ADC
+	const float R2 = R1 * (Vin / Vout - 1); // the calculated unknown resistance
+
+	return R2;
+}
+
+/**
+ * Waters the plant for a specified duration using a water pump and visual feedback through LED blinking.
+ *
+ * Activates a pump with some pwm power for some length of time
+ * blinks LED_BUILTIN to show it is actively trying to water a plant
+ *
+ * @param pumpPin - PWM pin to control pump
+ * @param wateringPwr - PWM power to water with
+ * @param wateringTime - length of time to water the plant
+ */
+void waterPlant(const int& pumpPin, const int& wateringPwr, const int& wateringTime) {
+	const unsigned long del = 50UL;
+
+	Serial.println("SLURP SLURP");
+
+	for (long start = millis(); millis() - start < wateringTime;) {
+
+		// Serial.print(".");
+		digitalWrite(LED_BUILTIN, millis() % (del*2) > del);
+		analogWrite(pumpPin, wateringPwr);
+
+	}
+	analogWrite(pumpPin, 0);
+}
+
+/**
+ * Adjust the watering threshold based on temperature and humidity conditions.
+ * @param tempC the current temperature in degrees Celsius
+ * @param humidity the current humidity level as a percentage
+ * @return adjusted watering threshold constrained between 20 and 60
+ */
+unsigned long adjustedWateringThreshold(const float& tempC, const float& humidity) {
+	int t = 35; // links show that 35% watering threshold looks like some kind of minimum
+
+	// btw these numbers are entirely arbitrary
+	if (tempC > 28) {
+		t +=8;
+	} else if (tempC < 18) {
+		t -= 5;
+	}
+
+	if (humidity < 40) {
+		t += 5;
+	} else if (humidity > 70) {
+		t -= 3;
+	}
+
+	return constrain(t, 20, 60);
 }
 
 void loop() {
 
-	/** STEP 1: LEARNING HOW TO USE THE DHT11
-	 * I put this here first because it is the simplest, most forgiving sensor, requiring only VCC, GND, and one digital pin
-	 * they can learn how to breadboard things here as well as a little bit electricity and reading schematics that you will show them
-	 */
+	// step 1: read the sensor, and see it's output before and after you breath on it
+	const float t = dht.readTemperature();
+	const float h = dht.readHumidity();
 
-	/** put on slide Deck
-	 * grab Single Wire Protocol screenshots from here for your slide deck it would be cool to show kids
-	 * https://www.mouser.com/datasheet/2/758/DHT11-Technical-Data-Sheet-Translated-Version-1143054.pdf
-	 */
+	// soil moisture is constantly active, which is bad for it but this is intentional
+	// PLA can trap moisture in infill as well as decay. The kit doesn't last too long hopefully
+	// use a capacitive soil moisture sensor in real life
 
-	/** understanding excersize for kids
-	 * do not use doubles in code, explain that floats are decimal points written in binary
-	 * have kids encode 32 bit IEEE754 floats manually, it sounds scarier than it is
-	 * help the kids understand NaN, Inf, decimals, and bit of math
-	 * helps the kids to understand why we are using NaN for errors
-	 */
+	const float soilResistance = resistorDivCalc(avgReading(RSTV_MSTR_PIN));
+	// step 2: read the resistance of wet and dry soil to calibrate soilPercent
+	const float soilMoisture = soilPercent(soilResistance);
 
-	// these functions are relatively simple, just give these to them
-	float t = dht.readTemperature();
-	float h = dht.readHumidity();
-
-	// t and h are NaN if there is an error, potentially indicating faulty wiring or some other error
-	// we should account for that in code anyway so if it triggers the Instructor knows to check on the kid
-	if (isnan(h) || isnan(t)) {
-		Serial.println("Failed to read from DHT sensor! Check your Wiring, or notify an Instructor.");
-		return; // restarts the void loop() to try again
+	if (!IS_VALID(t) || !IS_VALID(h) || !IS_VALID(soilResistance)) {
+		// error protection code, dw about this :)
+		// Serial.println("if this happens often, then call an instructor to repair a wiring issue");
+		return;
 	}
 
-	/** put on the slides
-	 * RH Scale
-	 * the RH scale is a measure of water dissolved in the air at a specific temperature
-	 * higher temperature means more water can dissolve in the air
-	 * 0% is no dissolved humidity in the air
-	 * 100% is the dew point, or the point where no more water can dissolve, so liquid condenses on surfaces
-	 * find some cool diagram or somn for this
-	 */
+	// reading all the sensors for graphing or other.
+	static unsigned long lastPrint = 0;
+	if (millis() - lastPrint > 500) {
+		Serial.print("Humidity: ");
+		Serial.print(h);
+		Serial.print("%,\tTemperature: ");
+		Serial.print(t);
+		Serial.print("*C,\tSoil Resistance: ");
+		Serial.print(soilResistance);
+		Serial.print("Ohms,\tSoil Moisture: ");
+		Serial.print(soilMoisture);
+		Serial.println("%");
+		lastPrint = millis();
+	}
 
-	/** kids excersize
-	 * have the kids find creative ways to display humidity and temperature, ex: a text based percentage bar for RH
-	 */
-	Serial.print("Humidity: ");
-	Serial.print(h);
-	Serial.print(" %\t");
-	Serial.print("Temperature: ");
-	Serial.print(t);
-	Serial.print(" *C\t");
+	static unsigned long lastWatering = 0;
+	const unsigned long WATERING_PERIOD = (1UL * 60UL * 60UL * 1000UL); // every 3 hours in ms
+	const unsigned long WATERING_TIME = 500; // water for .5 seconds, it helps to set this to 2 just to prime it
 
-	/** STEP 2: LEARNING HOW TO USE THE SOIL SENSOR
-	 * This is potentially more messy, less intuitive and more difficult to wire
-	 */
-
-	/** put in the slides
-	 * something to explain ohms law
-	 * electrons are in a super position of moving everywhere at once at 0V
-	 * applying a voltage shifts the electron waveform to move towards electron holes
-	 * when electrons encounter atoms and other electrons in their travels
-	 * they create electron interference waveforms (think the double slit experiment) and
-	 * the finite barrier problem (https://qm1.quantumtinkerer.tudelft.nl/8_finite-barrier/)
-	 * some electrons are reflected backwards, which creates resistance
-	 * now V = IR
-	 */
-
-	/** soil moisture sensor as a resistor
-	 * soil dissolved in water creates more free moving ions
-	 * which leads to less resistance for flowing electrons
-	 */
-
-	/** excersize for the kids to understand this shiii
-	 * have cups filled with soil with different levels of dampness
-	 * have kids measure the resistance of soil moisture sensor in every different cup with multimeter
-	 * have them free measure nothing on the soil sensor as well, write down all the resistance values for every cup so everyone can
-	 * look at all the datapoints to see what different dampness resistances look like so they can decide
-	 * ~ how much resistance triggers when the plant needs to be watered
-	 *
-	 * then have kids use 3.3V on the arduino, an LED, and try a bunch of different resistors to see how bright the LED is
-	 * don't let them plug in anything without a resistor things will fry
-	 */
-
-	 /** Resistor divider circuit
-	 * I would preface this by explaining a circuit diagram of what a resistor divider is
-	 * For Instructor/Curriculum guy/Andy:
-	 *		- a resistor divider is a circuit of 2 resistors in series, at the midpoint where they are connected has a calculatable output voltage
-	 *		- https://ohmslawcalculator.com/voltage-divider-calculator << show to kids for circuit
-	 *		- this is a ciruit used to find resistances, create voltage references from higher voltages, and step logic from higher to lower voltages
-	 *		- this equation assumes no current is drawn from the midpoint and not efficient for large currents, but useful for measurement and scaling
-	 *
-	 * This segment is about teaching AnalogRead, and The Resistor divider Circuit
-	 *
-	 * I made an Engineering Desgn decision to forgo the board that comes with the soil sensor and use a resistor divider to measure it's resistance
-	 * the boards only advantage is that it is a really weird component with a higher accuracy voltage comparator that we do not need
-	 *
-	 * have the kids try measuring extra resistors around 10k ohm with the resistor divider and code below to understand how the resistor divider measures
-	 * resistance
-	 */
-
-	const float R1 = 100000; // 10k Ohm
-	float Vin = 5.0; // voltage level of the resistor divider, il make a more clear documentation on how to word this for kids
-	float Vout = analogRead(RSTV_MSTR_PIN) * (5.0 / 1023.0);// freaky math 5 is from 5 volt, 1023 is maximum anologologologo ting
-	float R2 = R1 * (Vin / Vout - 1);
-
-	Serial.print("Resistance is: ");
-	Serial.print(R2);
-	Serial.println(" Ohms");
-
-	/**watering algorithm
-	 * this is more up to the kids the instructor and the plant
-	 * but my algorithm says
-	 * if soil is wet and watering_time has past then water the plant
-	 */
-	static unsigned long lastWatering = WATERING_TIME * .5;
-
-	if (R2 > WATERING_RESISTANCE && (millis() - lastWatering > WATERING_TIME)) {
+	if (!lastWatering || (millis() - lastWatering > WATERING_PERIOD && soilMoisture <= adjustedWateringThreshold(t,h))) {
+		waterPlant(PWM_PIN, WATERING_PWR, WATERING_TIME);
 
 		lastWatering = millis();
-		Serial.println("SLURP SLURP");
-
-		/** PWM pump control
-		 * use the S8050 transistor https://www.onsemi.com/download/data-sheet/pdf/ss8050-d.pdf
-		 * there are alternative transistors in the case of supplier issues, I believe the 2N2222 might also work
-		 * Arduino PWM pin to the Base
-		 * Emitter to GND
-		 * Base to GND cable of Pump
-		 * pump to Vcc +5V
-		 *
-		 * you can replace the pump with an LED to try out the circuit without spraying water everywhere
-		 * you can put this LED as a flyback diode to prevent future explosions after you kids figure the pump out
-		 * or put it in series with the pump with a resistor so you have a status LED when it waters
-		 */
-		analogWrite(PWM_PIN, WATERING_PWR);
-		delay(WATERING_OTHER_TIME);
-
-		analogWrite(PWM_PIN, 0);
 	}
-
 }
